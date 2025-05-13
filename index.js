c3eb32a2b8eaba8e31fcfeb7733aec4a1bf7203d
Psquare <pranavprajyot31@gmail.com>
2025-05-13 10:30:33 +0530

    debug commit

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.get('/api/products/:barcode', async (req, res) => {
     if (!product) {
       return res.status(404).json({ error: 'Product not found' });
     }
+    console.log(barcode);
     res.json(product);
   } catch (err) {
     res.status(500).json({ error: 'Internal server error' });
