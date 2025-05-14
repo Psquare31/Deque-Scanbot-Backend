eecc5067fc5cdd7a64e2fa7ededdac0a9452158a
Psquare <pranavprajyot31@gmail.com>
2025-05-14 18:38:22 +0530

    Update Product Schema

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,9 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   discount: { type: Number, default: 0 },
-  barcode: { type: String, required: true, unique: true }
+  barcode: { type: String, required: true, unique: true },
+  image_url: {type: String, required: false},
+  description: {type: String, required: false}
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
